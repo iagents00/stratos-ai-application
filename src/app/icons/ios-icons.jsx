78420be2bf2b1711eb4cd14ae92b87073ebfc547
Ivan Rodríguez Ruelas <ivanrodriguezruelas@MacBook-Air-de-Ivan.local>
2026-07-12 16:49:16 -0700

    Use UsersRound for CRM sidebar

--- src/app/icons/ios-icons.jsx
+++ src/app/icons/ios-icons.jsx
@@ -18,8 +18,8 @@ const G = {"c":{"s":"<path d=\"M336 256c-20.56 0-40.44-9.18-56-25.84-15.13-16.25
 
 const PREMIUM_ICON_OVERRIDES = {
   c: {
-    s: "<g transform=\"translate(32 32) scale(18.666667)\" fill=\"none\" stroke=\"currentColor\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"1.65\"><circle cx=\"12\" cy=\"8\" r=\"5\"/><path d=\"M20 21a8 8 0 0 0-16 0\"/></g>",
-    o: "<g transform=\"translate(32 32) scale(18.666667)\" fill=\"none\" stroke=\"currentColor\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"1.65\"><circle cx=\"12\" cy=\"8\" r=\"5\"/><path d=\"M20 21a8 8 0 0 0-16 0\"/></g>",
+    s: "<g transform=\"translate(32 32) scale(18.666667)\" fill=\"none\" stroke=\"currentColor\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"1.65\"><path d=\"M18 21a8 8 0 0 0-16 0\"/><circle cx=\"10\" cy=\"8\" r=\"5\"/><path d=\"M22 20c0-3.37-2-6.5-4-8a5 5 0 0 0-.45-8.3\"/></g>",
+    o: "<g transform=\"translate(32 32) scale(18.666667)\" fill=\"none\" stroke=\"currentColor\" stroke-linecap=\"round\" stroke-linejoin=\"round\" stroke-width=\"1.65\"><path d=\"M18 21a8 8 0 0 0-16 0\"/><circle cx=\"10\" cy=\"8\" r=\"5\"/><path d=\"M22 20c0-3.37-2-6.5-4-8a5 5 0 0 0-.45-8.3\"/></g>",
   },
   lp: {
     s: "<polygon points=\"256 64 422 160 422 352 256 448 90 352 90 160\" fill=\"currentColor\"/><polygon points=\"256 64 422 160 422 352 256 448 90 352 90 160\" fill=\"none\" stroke=\"currentColor\" stroke-width=\"24\" stroke-linejoin=\"round\"/>",
